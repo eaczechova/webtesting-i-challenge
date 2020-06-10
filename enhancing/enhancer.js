1f05ba86bbc119e7b0f67f44b671a1f256d82417
Ewa Czech <ewa.czech@hotmail.com>
2020-06-10 15:52:28 +0200

    add mvp test suites and functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,38 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get,
 };
 
 function succeed(item) {
-  return { ...item };
+	if (item.enhancement < 20) {
+		item = { ...item, enhancement: item.enhancement + 1 };
+		return item;
+	} else {
+		return { ...item };
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	if (item.enhancement < 15) {
+		return { ...item, durability: item.durability - 5 };
+	} else if (item.enhancement >= 15 && item.enhancement <= 16) {
+		return { ...item, durability: item.durability - 10 };
+	} else if (item.enhancement > 16) {
+		return {
+			...item,
+			durability: item.durability - 10,
+			enhancement: item.enhancement - 1,
+		};
+	}
 }
 
 function repair(item) {
-  return { ...item };
+	item.durability = 100;
+	return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
+
 // test away!
+describe('enhancer functions', () => {
+	describe('test repair function', () => {
+		it('restor item durability to 100', () => {
+			const item = {
+				name: 'Ewa',
+				enhancement: 16,
+				durability: 10,
+			};
+			repair(item);
+			expect(item.durability).toBe(100);
+		});
+	});
+
+	describe('test success function', () => {
+		it('return enhancement incresed by 1', () => {
+			const input = {
+				name: 'test',
+				enhancement: 14,
+				durability: 5,
+			};
+
+			const result = {
+				name: 'test',
+				enhancement: 15,
+				durability: 5,
+			};
+
+			const succeedResult = succeed(input);
+			expect(succeedResult.enhancement).toBe(result.enhancement);
+		});
+	});
+	describe('test fails function', () => {
+		it('return correct value for enhancement and durability', () => {
+			const input = {
+				name: 'test',
+				enhancement: 17,
+				durability: 15,
+			};
+
+			const result = {
+				name: 'test',
+				enhancement: 16,
+				durability: 5,
+			};
+
+			const failResult = fail(input);
+
+			expect(failResult).toEqual(result);
+		});
+	});
+});
